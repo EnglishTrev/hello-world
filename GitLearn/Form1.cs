9a7072c2343dc6e662603b3d2c206ebd567a73fe
Trevor <trevorwhitsey@gmail.com>
2016-10-15 15:02:39 +0100

    amended message

--- GitLearn/Form1.cs
+++ GitLearn/Form1.cs
@@ -19,7 +19,7 @@ namespace GitLearn
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world");
+            MessageBox.Show("Hello world develop");
         }
     }
 }
